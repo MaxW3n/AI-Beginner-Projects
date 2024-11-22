117eb432902e40f677119cadcb485fd99119642c
MaxW3n <maxsijia.wen@gmail.com>
2024-11-21 19:04:21 -0500

    Added dashcam video

--- Car Computer Vision/Car Vision.py
+++ Car Computer Vision/Car Vision.py
@@ -2,12 +2,12 @@ import cv2
 trained_car_data = cv2.CascadeClassifier('Car Computer Vision/cars.xml')
 trained_pedestrian_data = cv2.CascadeClassifier('Car Computer Vision/haarcascade_fullbody.xml')
 car_img = cv2.imread("Car Computer Vision/typical_view.jpg")
-dashcam = cv2.VideoCapture('Car Computer Vision/Angry Pedestrian Blocks Cyclist As He Races Through Zebra Crossing.mp4')
+dashcam = cv2.VideoCapture('Car Computer Vision/WATCH_ Dashcam footage emerges of Bedfordview shoot-out.mp4')
 
 while True:
     successframe, frame = dashcam.read()
     grayscale_vers = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    car_coordinates = trained_car_data.detectMultiScale(grayscale_vers, scaleFactor=1.1)
+    car_coordinates = trained_car_data.detectMultiScale(grayscale_vers, scaleFactor=1.2)
     pedes_coordinates = trained_pedestrian_data.detectMultiScale(grayscale_vers, scaleFactor=1.1)
     for (x,y,w,h) in car_coordinates:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
